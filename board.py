42cb86c55e5edc8165c284dd7d142dbd573ee641
informatique <informatique@B3-07-INFO1.TLPU161.etab.local>
2019-11-18 12:57:58 +0100

    toto

---
+++ board.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
+
+              #fenetre https://zestedesavoir.com/tutoriels/846/pygame-pour-les-zesteurs/1381_a-la-decouverte-de-pygame/creer-une-simple-fenetre-personnalisable/
+import pygame
+
+pygame.init()
+
+ecran = pygame.display.set_mode((300, 200))
+
+pygame.quit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Board (object):
+    def __init__(self,size):
+        self.size=size
+
