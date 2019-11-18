a4f2885dc2fc5c58771af216880ca4d04a3f1e09
informatique <informatique@B3-07-INFO1.TLPU161.etab.local>
2019-11-18 13:33:58 +0100

    board test 1

--- board.py
+++ board.py
@@ -10,9 +10,19 @@ import pygame
 
 pygame.init()
 
-ecran = pygame.display.set_mode((300, 200))
+ecran = pygame.display.set_mode((300,200))
+
+continuer = True
+
+while continuer :
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            continuer = False
 
 pygame.quit()
+
+
+
 
 
 
