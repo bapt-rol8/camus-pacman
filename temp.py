4c323af4a2baa3e6f2c396ab129f59302369b43b
auriane.gonindard <auriane.gonindard@tlpu161camus.onmicrosoft.com>
2020-01-06 13:31:33 +0100

    positionnement

--- temp.py
+++ temp.py
@@ -18,21 +18,31 @@ fenetre = pygame.display.set_mode((640, 480))
 perso = pygame.image.load("perso.png").convert_alpha()
 wall = pygame.image.load("mur.jpg")
 wall2 = pygame.image.load("mur.jpg")
-fraise = pygame.image.load("fraise.jpg")
+fraise = pygame.image.load("fraise3.png")
+banane = pygame.image.load("banane.png")
+lampe = pygame.image.load("lampe.png")
 position_perso = perso.get_rect()
 position_wall = wall.get_rect()
 position_wall2 = wall2.get_rect()
 position_fraise = fraise.get_rect()
+position_banane = banane.get_rect()
+position_lampe = lampe.get_rect()
 position_wall.x = 200
 position_wall.y = 200
 position_wall2.x = 100
 position_wall2.y = 100
 position_fraise.y = 269
 position_fraise.x = 512
+position_banane.x = 412
+position_banane.y = 412
+position_lampe.x = 312
+position_lampe.y = 312
 fenetre.blit(perso, position_perso)
 fenetre.blit(wall, position_wall)
 fenetre.blit(wall2, position_wall2)
 fenetre.blit(fraise,position_fraise)
+fenetre.blit(banane,position_banane)
+fenetre.blit(lampe,position_lampe)
 #Rafraîchissement de l'écran
 pygame.display.flip()
 
@@ -41,29 +51,31 @@ pygame.key.set_repeat(10,10)
 continuer = 1
 while continuer:
     for event in pygame.event.get():    #Attente des événements
-        if event.type == QUIT:
+        if event.type == pygame.QUIT:
             continuer = 0
-        if event.type == KEYDOWN:
-            if event.key == K_DOWN:    #Si "flèche bas"
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:    #Si "flèche bas"
                 #On descend le perso
                 position_perso = position_perso.move(0,3)
-            if event.key == K_UP:    #Si "flèche haut"
+            if event.key == pygame.K_UP:    #Si "flèche haut"
                 #On monte le perso
                 position_perso = position_perso.move(0,-3)
-            if event.key == K_RIGHT:    #Si "flèche droite"
+            if event.key == pygame.K_RIGHT:    #Si "flèche droite"
                 #On descend le perso
                 position_perso = position_perso.move(3,0)
-            if event.key == K_LEFT:    #Si "flèche gauche"
+            if event.key == pygame.K_LEFT:    #Si "flèche gauche"
                 #On descend le perso
                 position_perso = position_perso.move(-3,0)
     
     
-    fenetre.fill((55,86,34))
+    fenetre.fill((150,0,255))
     #Re-collage
     fenetre.blit(perso, position_perso)
     fenetre.blit(wall, position_wall)
     fenetre.blit(wall2, position_wall2)
     fenetre.blit(fraise,position_fraise)
+    fenetre.blit(banane,position_banane)
+    fenetre.blit(lampe,position_lampe)
     #Rafraichissement
     pygame.display.flip()
     
